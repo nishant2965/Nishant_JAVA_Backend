a483596f41977d6020587f28ae3d027e0fef7aff
Nishant <nag2965@gmail.com>
2023-05-31 16:58:08 -0500

    Ass 2

--- DAY 2/Student.java
+++ DAY 2/Student.java
@@ -1,11 +1,27 @@
 public class Student{
 
+    public static String Name = "Nishant";
 
-public static void main(String[] aggs){
-    
-}
+
 
 public static String getName(){
+    // String name = Name;
+    return Name;
+}
+
+
+public static String setName(String name){
+    Name = name;
+    return Name;
+}
+
+public static void main(String[] aggs){
+
+    System.out.println(getName());
+
+    System.out.println(setName("Agrawal"));
+    
+
     
 }
 
