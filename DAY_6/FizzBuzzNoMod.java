71d03be90bfd1249f5a4ebaf0cb7fc636e1fcb78
Nishant <nag2965@gmail.com>
2023-06-05 19:34:57 -0500

    Create FizzBuzzNoMod.java

---
+++ DAY_6/FizzBuzzNoMod.java
@@ -0,0 +1,29 @@
+
+public class FizzBuzzNoMod {
+
+    public static void main(String[] args){
+        int a = 0,b=0;
+
+        int n = 100;
+        for(int i =1; i<=100; i++){
+            a++;
+            b++;
+
+
+            if(a == 3){
+                System.out.print("Fizz");
+                a = 0;
+            }
+            if(b == 5){
+                System.out.print("Buzz");
+                b = 0;
+            }
+            if(a!= 0 && b!= 0){
+                System.out.print(i);
+            }
+            System.out.print(" ");
+        }
+
+    }
+    
+}
